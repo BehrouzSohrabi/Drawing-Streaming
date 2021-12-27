a80e7bd0354dd1b9aa3814e2df18de004d065c0d
Behrouz Sohrabi <sohrabi.beh@gmail.com>
2021-12-26 21:29:56 -0700

    added gitignore, cutting path

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -2,20 +2,11 @@ import { combineReducers } from "redux";
 import { ActionType, Action } from "../actions/actions";
 import Paper from "paper";
 
-const reducer = (drawing: any = {}, action: Action): any => {
+const drawingReducer = (drawing: any = {}, action: Action): any => {
   switch (action.type) {
     case ActionType.draw:
-      drawing = {
-        path: new Paper.Path(),
-        jsons: {},
-        lastSeg: [],
-        textItem: new Paper.PointText(new Paper.Point(20, 30)),
-        segmentsToSend: {}
-      };
-      drawing.textItem.fillColor = new Paper.Color("black");
-      drawing.textItem.content = "Click and drag to draw a line.";
       let removeItems = function removeItems(input: [], item: number) {
-        var arr = [];
+        var arr:any = [];
         for (var i = item - 1; i < input.length; i++) {
           arr.push(input[i]);
         }
@@ -26,19 +17,17 @@ const reducer = (drawing: any = {}, action: Action): any => {
         if (drawing.duration < 100) {
           drawing.duration = 100;
         }
-        var segmentCount = drawing.path.segments.length;
+        var segmentCount = path.segments.length;
         drawing.textItem.content = "Segment count: " + segmentCount;
-        // path.simplify(20);
-        var newSegmentCount = drawing.path.segments.length;
+        path.simplify(20);
+        var newSegmentCount = path.segments.length;
         var difference = segmentCount - newSegmentCount;
-        var percentage =
-          100 - Math.round((newSegmentCount / segmentCount) * 100);
         drawing.textItem.content =
           difference +
           " of the " +
           segmentCount +
           " segments were removed. Saving " +
-          percentage +
+          (Math.round((1-(newSegmentCount / segmentCount)) * 100)) +
           "%";
         drawing.jsons = Paper.project.exportJSON({ asString: false });
         var segments =
@@ -46,6 +35,8 @@ const reducer = (drawing: any = {}, action: Action): any => {
             drawing.jsons[0][1]["children"].length - 1
           ][1]["segments"];
 
+          console.log(drawing.jsons[0][1]["children"])
+          console.log(segments)
         if (drawing.lastCut !== 0) {
           drawing.segmentsToSend = removeItems(segments, drawing.lastCut);
         } else {
@@ -58,26 +49,35 @@ const reducer = (drawing: any = {}, action: Action): any => {
           drawing.start = Date.now();
           drawing.lastCut = segments.length;
         }
-        console.log(JSON.stringify(segments));
-        console.log(JSON.stringify(drawing.segmentsToSend));
-        console.log(drawing.lastCut);
-        drawing.toSend = JSON.stringify(drawing.segmentsToSend);
+        // console.log(JSON.stringify(segments));
+        // console.log(JSON.stringify(drawing.segmentsToSend));
+        // console.log(drawing.lastCut);
+        // drawing.toSend = JSON.stringify(drawing.segmentsToSend);
         // console.log(getByteCount(drawing.toSend) + " BYTES");
         // exportPath(toSend, duration);
       };
-
-      Paper.view.onMouseDown = function (event) {
-        drawing.path.strokeColor = new Paper.Color("black");
-        drawing.path.strokeWidth = 3;
+      drawing = {
+        jsons: {},
+        lastSeg: [],
+        textItem: new Paper.PointText(new Paper.Point(20, 30)),
+        segmentsToSend: {}
+      };
+      drawing.textItem.fillColor = new Paper.Color("black");
+      drawing.textItem.content = "Click and drag to draw a line.";
+      let path:any;
+      Paper.view.onMouseDown = function () {
+        path = new Paper.Path();
+        path.strokeColor = new Paper.Color("black");
+        path.strokeWidth = 3;
         drawing.start = Date.now();
         drawing.interval = setInterval(function () {
           cutPath();
         }, 500);
       };
-      Paper.view.onMouseDrag = function (event) {
-        drawing.path.add(event.point);
+      Paper.view.onMouseDrag = function (event: any) {
+        path.add(event.point);
       };
-      Paper.view.onMouseUp = function (event) {
+      Paper.view.onMouseUp = function (event: any) {
         cutPath(true);
         clearInterval(drawing.interval);
       };
@@ -105,7 +105,7 @@ const reducer = (drawing: any = {}, action: Action): any => {
 };
 
 const reducers = combineReducers({
-  drawing: reducer
+  drawing: drawingReducer
 });
 
 export default reducers;
